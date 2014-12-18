fe3d1acea6287e948ee247d3bba6c992c27dc943
Po_Hsun <showoowohs@google.com>
2014-12-17 23:56:39 -0600

    判斷電信符號, 比方說3G 4G ...

--- IMT8_CarriersWidget/src/com/imobile/widget_carriers/MyWidgetProvider.java
+++ IMT8_CarriersWidget/src/com/imobile/widget_carriers/MyWidgetProvider.java
@@ -18,7 +18,40 @@ import android.widget.Toast;
 public class MyWidgetProvider extends AppWidgetProvider {
 
 	private static final String CLICK_ACTION = "myCustomAction";
-
+	/**
+	 * GSM = 0
+	 */
+	private final int GSM = 0; 
+	/**
+	 * GSM_COMPACT = 1
+	 */
+	private final int GSM_COMPACT = 1; 
+	/**
+	 * UTRAN = 2
+	 */
+	private final int UTRAN = 2; 
+	/**
+	 * GSM_with_EDGE_availability = 3
+	 */
+	private final int GSM_with_EDGE_availability = 3;
+	/**
+	 * UTRAN_with_HSDPA_availability = 4
+	 */
+	private final int UTRAN_with_HSDPA_availability = 4; 
+	/**
+	 * UTRAN_with_HSUPA_availability = 5
+	 */
+	private final int UTRAN_with_HSUPA_availability = 5; 
+	/**
+	 * UTRAN_with_HSDPA_and_HSUPA_availability = 6
+	 */
+	private final int UTRAN_with_HSDPA_and_HSUPA_availability = 6;
+	/**
+	 * LTE = 7
+	 */
+	private final int LTE = 7; 
+	private String Po_Signal = ""; 
+	 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		super.onReceive(context, intent);
@@ -33,7 +66,7 @@ public class MyWidgetProvider extends AppWidgetProvider {
 		}
 
 	}
-
+	
 	@Override
 	public void onEnabled(Context context) {
 		super.onEnabled(context);
@@ -78,9 +111,9 @@ public class MyWidgetProvider extends AppWidgetProvider {
 			File sdcard = Environment.getExternalStorageDirectory();
 
 			// Get the text file
-//			File file = new File(sdcard, "111.txt");
+			// File file = new File(sdcard, "111.txt");
 			File file = new File(sdcard, "ublox_SIM_info.txt");
-			Log.d("Po_add", "file="+file);
+			Log.d("Po_add", "file=" + file);
 
 			// Read text from file
 			StringBuilder text = new StringBuilder();
@@ -93,10 +126,11 @@ public class MyWidgetProvider extends AppWidgetProvider {
 			}
 			// get 電信商資料
 			Carrier += Split_Carrier(text.toString());
-			Log.d("Po_add", "Carrier="+Carrier);
+			Log.d("Po_add", "Carrier=" + Carrier);
 			// System.out.println("Po...text=" + text);
+			this.Po_Signal = Split_radio_access_technology(text.toString());
 		} catch (IOException e) {
-			Log.d("Po_add", "IOException"+e);
+			Log.d("Po_add", "IOException" + e);
 			// You'll need to add proper error handling here
 		}
 
@@ -125,14 +159,73 @@ public class MyWidgetProvider extends AppWidgetProvider {
 
 			// 第3參數舊式電信業者資料
 			if (i == 2) {
-				//System.out.println("Po_split[2]=" + AfterSplit[i]);
-				//將取得的電信業者資料中的怪怪符號拿除
+				// System.out.println("Po_split[2]=" + AfterSplit[i]);
+				// 將取得的電信業者資料中的怪怪符號拿除
 				String Del_Symbol = "";
-				int last = AfterSplit[i].length()-1;
+				int last = AfterSplit[i].length() - 1;
 				Del_Symbol = AfterSplit[i].substring(1, last);
 				Log.d("Po_add", "Del_Symbol=" + Del_Symbol);
 				// 回傳電信業者資料
 				return Del_Symbol;
+			}
+		}
+
+		return "";
+	}
+
+	/****
+	 * 找出電信業者資資料的 radio access technology 並回傳 
+	 * Indicates the radio access technology
+	 *  
+	 *  0: GSM 
+	 *  1: GSM COMPACT 
+	 *  2: UTRAN 
+	 *  3: GSM with EDGE availability 
+	 *  4: UTRAN with HSDPA availability 
+	 *  5: UTRAN with HSUPA availability 
+	 *  6: UTRAN with HSDPA and HSUPA availability 
+	 *  7: LTE
+	 * 
+	 * @param str
+	 * @return
+	 */
+	private String Split_radio_access_technology(String str) {
+		Log.d("Po_add", "Split_radio_access_technology()");
+		// +COPS: 0,0,Chunghwa Telecom,2
+		String mustSplitString = str;
+
+		String[] AfterSplit = mustSplitString.split(",");
+
+		for (int i = 0; i < AfterSplit.length; i++) {
+
+			// 找到radio access technology
+			if (i == 3) {
+				
+				String tmp = AfterSplit[i].substring(0, 1);
+				int radio_access_technology = Integer.parseInt(tmp);
+				//radio_access_technology
+				Log.d("Po_add", "radio_access_technology="+ radio_access_technology);
+				//reference url  http://gophone.pixnet.net/blog/post/167676570-lte%E3%80%81wcdma%E3%80%81hsupa%E3%80%81gsm%E3%80%80%E5%82%BB%E5%82%BB%E5%88%86%E4%B8%8D%E6%B8%85%EF%BC%9F-%E7%B6%9C%E5%90%88%E8%A7%A3%E6%9E%90
+				switch (radio_access_technology) {
+					case GSM:
+						Log.d("Po_add", "2G");
+						return "GSM";
+					case GSM_COMPACT:
+					case UTRAN:
+					case GSM_with_EDGE_availability:
+					case UTRAN_with_HSDPA_availability:
+					case UTRAN_with_HSUPA_availability:
+					case UTRAN_with_HSDPA_and_HSUPA_availability:
+						Log.d("Po_add", "3G");
+						return "3G";
+					case LTE:
+						Log.d("Po_add", "4G");
+						return "4G";
+					default:
+						Log.d("Po_add", "default");
+				}
+								
+				return "";
 			}
 		}
 
