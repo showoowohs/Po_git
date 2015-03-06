653d22dc3ecd311178f8d3f6a3e90e747b836cf6
Po_Hsun <showoowohs@google.com>
2015-03-06 10:16:02 -0600

    modify ./IQ8_FixWiFi/AndroidManifest.xml & ./IQ8_FixWiFi/src/com/imobile/iq8_fixwifi/MainActivity.java & add ./IQ8_FixWiFi/ReadMe.md & add ./IQ8_FixWiFi/assets/, 完成 copy WCNSS_qcom_cfg.ini & WCNSS_qcom_cfg_org.ini to /mnt/shell/emulated/0

---
+++ IQ8_FixWiFi/ReadMe.md
@@ -0,0 +1,6 @@
+# ReadMe
+1. 
+
+### reference:
+> 1. [copy file from assets](http://www.technotalkative.com/android-copy-files-from-assets-to-sd-card/)
+> 2. [git-hub](https://github.com/PareshMayani/Android-CopyFiles)
--- IQ8_FixWiFi/src/com/imobile/iq8_fixwifi/MainActivity.java
+++ IQ8_FixWiFi/src/com/imobile/iq8_fixwifi/MainActivity.java
@@ -1,16 +1,71 @@
 package com.imobile.iq8_fixwifi;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	final String TAG = "Po_debug";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// 1. copy file to /sdcard
+		CopyAssets();
+	}
+
+	private void CopyAssets() {
+		AssetManager assetManager = getAssets();
+		String[] files = null;
+		try {
+			files = assetManager.list("Files");
+		} catch (IOException e) {
+			Log.e(TAG, e.getMessage());
+		}
+
+		for (String filename : files) {
+			Log.i(TAG, "File name => " + filename);
+			InputStream in = null;
+			OutputStream out = null;
+			try {
+				in = assetManager.open("Files/" + filename); // if files resides
+																// inside the
+																// "Files"
+																// directory
+																// itself
+				out = new FileOutputStream(Environment
+						.getExternalStorageDirectory().toString()
+						+ "/"
+						+ filename);
+				copyFile(in, out);
+				in.close();
+				in = null;
+				out.flush();
+				out.close();
+				out = null;
+			} catch (Exception e) {
+				Log.i(TAG, e.getMessage());
+			}
+		}
+	}
+
+	private void copyFile(InputStream in, OutputStream out) throws IOException {
+		byte[] buffer = new byte[1024];
+		int read;
+		while ((read = in.read(buffer)) != -1) {
+			out.write(buffer, 0, read);
+		}
 	}
 
 	@Override
