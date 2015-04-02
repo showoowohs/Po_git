ab2a3268ad2c1ee785d557b3ff1d4fb144a66190
Po_Hsun <showoowohs@google.com>
2015-04-02 10:07:31 -0600

    準備要放棄透過 JNI run script這件事, 雖然透過JNI 可以 run script , 但是似乎沒有權限透過 script write data 到別得地方

--- IQ8ReadSerialNumber/jni/com_imobile_iq8readserialnumber_iMobileReadSerialNumber.c
+++ IQ8ReadSerialNumber/jni/com_imobile_iq8readserialnumber_iMobileReadSerialNumber.c
@@ -24,9 +24,10 @@ extern "C"
 {
 #endif
 
+int system(const char *command);
 
-JNIEXPORT jstring JNICALL Java_com_imobile_iq8readserialnumber_iMobileReadSerialNumber_HelloWorld(JNIEnv *env, jclass arg)
-{
+JNIEXPORT jstring JNICALL Java_com_imobile_iq8readserialnumber_iMobileReadSerialNumber_HelloWorld(
+		JNIEnv *env, jclass arg) {
 	jstring str = (*env)->NewStringUTF(env, "HelloWorld from JNI !");
 
 	//Po log start
@@ -34,16 +35,40 @@ JNIEXPORT jstring JNICALL Java_com_imobile_iq8readserialnumber_iMobileReadSerial
 	return str;
 }
 
-JNIEXPORT jstring JNICALL Java_com_imobile_iq8readserialnumber_iMobileReadSerialNumber_ReadSN(JNIEnv *env, jclass arg)
-{
+JNIEXPORT jstring JNICALL Java_com_imobile_iq8readserialnumber_iMobileReadSerialNumber_ReadSN(
+		JNIEnv *env, jclass arg) {
+
+
+	// ls -al | grep '^d'
+	FILE *pp;
+	pp = popen("/system/IQ8_Read_SN.sh", "r");
+	if (pp != NULL) {
+		while (1) {
+			char *line;
+			char buf[1000];
+			line = fgets(buf, sizeof buf, pp);
+			if (line == NULL)
+				break;
+			if (line[0] == 'd')
+				LOGI("[Po add]%s", line); // line includes '\n'
+		}
+		pclose(pp);
+	}
+
+
+
+	 //system("/system/IQ8_Read_SN.sh");
+
+
+
+
+	LOGI("[Po add] ReadSN() 111");
 	jstring str = (*env)->NewStringUTF(env, "ReadSN from JNI !");
 
 	//Po log start
-	LOGI("[Po add] ReadSN()");
+	LOGI("[Po add] ReadSN() 222");
 	return str;
 }
-
-
 
 #ifdef __cplusplus
 }
