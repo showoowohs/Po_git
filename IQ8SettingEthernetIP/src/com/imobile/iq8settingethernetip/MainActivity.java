5342fb467511f3fabe66d3736f4d96142be0486f
Po_Hsun <showoowohs@google.com>
2015-04-20 16:42:36 -0600

    modify ./IQ8SettingEthernetIP/src/com/imobile/iq8settingethernetip/MainActivity.java, 完成 recovery config function, 按下 button Default_config 以後 can delete /mnt/shell/emulated/0/IQ8_EthernetIP.sh , 這樣一來 dns.sh 就會執行 IQ8 default 的 script(在我的測試環境這樣絕對能上 internet)

--- IQ8SettingEthernetIP/src/com/imobile/iq8settingethernetip/MainActivity.java
+++ IQ8SettingEthernetIP/src/com/imobile/iq8settingethernetip/MainActivity.java
@@ -1,15 +1,24 @@
 package com.imobile.iq8settingethernetip;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private final String TAG = "Po_ETH";
+	private int Default_config = 9;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,64 @@ public class MainActivity extends Activity {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	protected void onDestroy() {
+		Log.d(TAG, "exit APP");
+		int pid = android.os.Process.myPid();
+		android.os.Process.killProcess(pid);
+		super.onDestroy();
+	}
+
+	/***
+	 * 顯示diglog
+	 * 
+	 * @param Title
+	 * @param Msg
+	 * @param event
+	 */
+	public void show_dialog(String Title, String Msg, int Po_event) {
+		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+		dialog.setTitle(Title);
+		dialog.setMessage(Msg);
+		dialog.setIcon(android.R.drawable.ic_dialog_alert);
+		dialog.setCancelable(false);
+		
+		if(Po_event == this.Default_config){
+			dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				
+				public void onClick(DialogInterface dialog, int which) {
+					
+					onDestroy();
+				}
+			});
+		}else{
+			dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				
+				public void onClick(DialogInterface dialog, int which) {
+					
+					// 按下PositiveButton要做的事
+					Toast.makeText(MainActivity.this, "APP is exit",
+							Toast.LENGTH_SHORT).show();
+					onDestroy();
+				}
+			});
+		}
+		dialog.show();
+
+	}
+	
+	/***
+	 * Po_set_default_config is a button, can delete /sdcard/IQ8_EthernetIP.sh
+	 * @param view
+	 */
+	public void Po_set_default_config(View view) {
+		Log.d(TAG, "Po_set_default_config()");
+		File delete_file = new File("/sdcard/IQ8_EthernetIP.sh");
+		// delete file
+		delete_file.delete();
+		show_dialog("Restore config", "success", this.Default_config);
 	}
 
 	@Override
