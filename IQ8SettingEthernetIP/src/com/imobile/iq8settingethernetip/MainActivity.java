cc06e5c34e61518a82c2bec2a58178a721e35463
Po_Hsun <showoowohs@google.com>
2015-04-20 17:33:53 -0600

    modify ./IQ8SettingEthernetIP/src/com/imobile/iq8settingethernetip/MainActivity.java, 完成 read IP (uesr input) function

--- IQ8SettingEthernetIP/src/com/imobile/iq8settingethernetip/MainActivity.java
+++ IQ8SettingEthernetIP/src/com/imobile/iq8settingethernetip/MainActivity.java
@@ -13,18 +13,79 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	private final String TAG = "Po_ETH";
 	private int Default_config = 9;
+	// Po_IP_area
+	private EditText Po_IP_1, Po_IP_2, Po_IP_3, Po_IP_4;
+	// Po_Gateway_area
+	private EditText Po_GW_1, Po_GW_2, Po_GW_3, Po_GW_4;
+	// Po_DNS_area
+	private EditText Po_DNS_1, Po_DNS_2, Po_DNS_3, Po_DNS_4;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		Po_find_id();
+	}
+
+	private void Po_find_id() {
+
+		// find id IP area
+		this.Po_IP_1 = (EditText) findViewById(R.id.Po_IP_1);
+		this.Po_IP_2 = (EditText) findViewById(R.id.Po_IP_2);
+		this.Po_IP_3 = (EditText) findViewById(R.id.Po_IP_3);
+		this.Po_IP_4 = (EditText) findViewById(R.id.Po_IP_4);
+
+		// find id Gateway area
+		this.Po_GW_1 = (EditText) findViewById(R.id.Po_GW_1);
+		this.Po_GW_2 = (EditText) findViewById(R.id.Po_GW_2);
+		this.Po_GW_3 = (EditText) findViewById(R.id.Po_GW_3);
+		this.Po_GW_4 = (EditText) findViewById(R.id.Po_GW_4);
+
+		// find id Gateway area
+		this.Po_DNS_1 = (EditText) findViewById(R.id.Po_DNS_1);
+		this.Po_DNS_2 = (EditText) findViewById(R.id.Po_DNS_2);
+		this.Po_DNS_3 = (EditText) findViewById(R.id.Po_DNS_3);
+		this.Po_DNS_4 = (EditText) findViewById(R.id.Po_DNS_4);
+
+	}
+
+	/***
+	 * Read user input IP(if error can return X)
+	 * @return IP
+	 */
+	private String Read_IP() {
+
+		String IP_address = "";
+		String PoTmp_IP1, PoTmp_IP2, PoTmp_IP3, PoTmp_IP4;
+		PoTmp_IP1 = this.Po_IP_1.getText().toString();
+		PoTmp_IP2 = this.Po_IP_2.getText().toString();
+		PoTmp_IP3 = this.Po_IP_3.getText().toString();
+		PoTmp_IP4 = this.Po_IP_4.getText().toString();
+
+		if ((!PoTmp_IP1.equals("")) & (!PoTmp_IP2.equals(""))
+				& (!PoTmp_IP3.equals("")) & (!PoTmp_IP4.equals(""))) {
+
+			IP_address = PoTmp_IP1 + "." + PoTmp_IP2 + "." + PoTmp_IP3 + "."
+					+ PoTmp_IP4;
+
+		} else {
+//			Log.i(TAG, "IP_address is null");
+			return "X";
+		}
+		Log.i(TAG, "IP_address=" + IP_address);
+
+		return IP_address;
+	}
+
+	private void write_file() {
 		try {
 			// Path ==> /mnt/shell/emulated/0/
 			FileWriter fw = new FileWriter("/sdcard/IQ8_EthernetIP.sh", false);
@@ -38,7 +99,7 @@ public class MainActivity extends Activity {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		Log.d(TAG, "exit APP");
@@ -60,33 +121,47 @@ public class MainActivity extends Activity {
 		dialog.setMessage(Msg);
 		dialog.setIcon(android.R.drawable.ic_dialog_alert);
 		dialog.setCancelable(false);
-		
-		if(Po_event == this.Default_config){
-			dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-				
-				public void onClick(DialogInterface dialog, int which) {
-					
-					onDestroy();
-				}
-			});
-		}else{
-			dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-				
-				public void onClick(DialogInterface dialog, int which) {
-					
-					// 按下PositiveButton要做的事
-					Toast.makeText(MainActivity.this, "APP is exit",
-							Toast.LENGTH_SHORT).show();
-					onDestroy();
-				}
-			});
+
+		if (Po_event == this.Default_config) {
+			dialog.setPositiveButton("OK",
+					new DialogInterface.OnClickListener() {
+
+						public void onClick(DialogInterface dialog, int which) {
+
+							onDestroy();
+						}
+					});
+		} else {
+			dialog.setPositiveButton("OK",
+					new DialogInterface.OnClickListener() {
+
+						public void onClick(DialogInterface dialog, int which) {
+
+							// 按下PositiveButton要做的事
+							Toast.makeText(MainActivity.this, "APP is exit",
+									Toast.LENGTH_SHORT).show();
+							onDestroy();
+						}
+					});
 		}
 		dialog.show();
 
 	}
-	
+
 	/***
 	 * Po_set_default_config is a button, can delete /sdcard/IQ8_EthernetIP.sh
+	 * 
+	 * @param view
+	 */
+	public void Po_save_config(View view) {
+		Log.d(TAG, "Po_save_config()");
+		Read_IP();
+		// show_dialog("Restore config", "success", this.Default_config);
+	}
+
+	/***
+	 * Po_set_default_config is a button, can delete /sdcard/IQ8_EthernetIP.sh
+	 * 
 	 * @param view
 	 */
 	public void Po_set_default_config(View view) {
