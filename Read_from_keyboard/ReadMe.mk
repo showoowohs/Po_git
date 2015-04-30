c73fedfde52f645cfd1d6a2f444a471b34ccaf87
Po_Hsun <showoowohs@google.com>
2015-04-30 11:48:52 -0600

    modify ./Read_from_keyboard/ReadMe.mk, add photo

--- Read_from_keyboard/ReadMe.mk
+++ Read_from_keyboard/ReadMe.mk
@@ -1,2 +1,2 @@
 result:
-![alt tag](https://github.com/showoowohs/Po_git/blob/PoTestJNI/PoTestJNI/result.png)
+![alt tag](https://github.com/showoowohs/Po_git/blob/master/Read_from_keyboard/output_Dc1cqP.gif)
