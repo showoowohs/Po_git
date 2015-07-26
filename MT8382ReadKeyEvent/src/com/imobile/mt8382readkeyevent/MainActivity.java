e3240e9f612efe05ad4594999cb0edccb6296fd3
Po_Hsun <showoowohs@google.com>
2015-07-26 17:07:11 -0600

    modify ./MT8382ReadKeyEvent/res/layout/activity_main.xml & ./MT8382ReadKeyEvent/src/com/imobile/mt8382readkeyevent/MainActivity.java, done show UI function, if press hotkey can show message to UI, trun off UIthread

--- MT8382ReadKeyEvent/src/com/imobile/mt8382readkeyevent/MainActivity.java
+++ MT8382ReadKeyEvent/src/com/imobile/mt8382readkeyevent/MainActivity.java
@@ -1,11 +1,13 @@
 package com.imobile.mt8382readkeyevent;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -15,20 +17,89 @@ public class MainActivity extends Activity {
 	final int F14 = 184; // 0x00b8
 	final int F15 = 185; // 0x00b9
 	final int F16 = 186; // 0x00ba
-	private TextView Po_show_key;
+	TextView Po_show_key;
+	private Thread Updata_UI;
+	int PoKeyCode;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Po_findID();
+		// Updata_UI = new Updata_UI();
+		// Updata_UI.start();
 	}
 
 	private void Po_findID() {
-		TextView Po_show_key = (TextView) findViewById(R.id.Po_show_key);
+		Po_show_key = (TextView) findViewById(R.id.Po_show_key);
 		// setting TextView
-		Po_show_key.setText("You not press button");
+		this.Po_show_key.setText("You not press button");
 	}
+	
+	/*
+	private Handler mHandler = new Handler() {
+
+		public void handleMessage(Message msg) {
+
+			super.handleMessage(msg);
+
+			Log.d(TAG, "msg=" + msg.arg1);
+			if (msg != null) {
+				if (msg.arg1 != 0) {
+					Po_show_key.setText("You press button " + msg.arg1);
+				} else {
+					Po_show_key.setText("You not press button");
+				}
+			}
+
+		}
+
+	};
+
+	class Updata_UI extends Thread {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			super.run();
+			try {
+				while (true) {
+					Log.d(TAG, "Updata_UI");
+					Message msg = new Message();
+
+					switch (PoKeyCode) {
+					case F13:
+						msg.arg1 = 1;
+					case F14:
+						msg.arg1 = 2;
+					case F15:
+						msg.arg1 = 3;
+					case F16:
+						msg.arg1 = 4;
+					default:
+						msg.arg1 = 0;
+					}
+
+					mHandler.sendMessage(msg);
+					Thread.sleep(1000);
+				}
+			} catch (InterruptedException e) {
+				Log.d("TAG", "Thread was inturrupted");
+			} catch (Exception e) {
+				e.printStackTrace();
+				Log.d("TAG", "Thread was Exception");
+			}
+		}
+	}
+	*/
+
+	/*
+	 @Override
+	 public void onDestroy() {
+	 Updata_UI.interrupt();
+	 super.onDestroy();
+	 }
+	 */
 
 	public String Po_split_keycode(String keyevent) {
 		Log.d(TAG, "Po_split_keycode start");
@@ -61,7 +132,32 @@ public class MainActivity extends Activity {
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		Log.d(TAG, "start onKeyDown ok");
 		// Po_split_keycode(event.toString());
-		Log.d(TAG, "Po_split_keycode = " + Po_split_keycode(event.toString()));
+		// Log.d(TAG, "Po_split_keycode = " +
+		// Po_split_keycode(event.toString()));
+		String Po_Keycode = Po_split_keycode(event.toString());
+		if (Po_Keycode.equals("ERROR")) {
+			Log.d(TAG, "not thing");
+		} else {
+			// show UI
+			PoKeyCode = Integer.parseInt(Po_Keycode);
+			Log.d(TAG, "PoKeyCode = " + PoKeyCode);
+			switch (PoKeyCode) {
+			case F13:
+				Po_show_key.setText("You press button 1");
+				return true;
+			case F14:
+				Po_show_key.setText("You press button 2");
+				return true;
+			case F15:
+				Po_show_key.setText("You press button 3");
+				return true;
+			case F16:
+				Po_show_key.setText("You press button 4");
+				return true;
+			default:
+				Po_show_key.setText("You not press button");
+			}
+		}
 
 		// *** call standard android API ***//
 		// switch (keyCode) {
