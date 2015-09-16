0b119190930b49013f35e594cd8bb7cdd7c724d2
Po_Hsun <showoowohs@google.com>
2015-09-16 10:33:10 -0600

    加入 on/off icon

--- MT8382ToggleADBandUSB/src/com/imobile/mt8382toggleadbandusb/MainActivity.java
+++ MT8382ToggleADBandUSB/src/com/imobile/mt8382toggleadbandusb/MainActivity.java
@@ -2,33 +2,25 @@ package com.imobile.mt8382toggleadbandusb;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	private TextView POTV1, POTV2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		PO_findID();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	
+	private void PO_findID(){
+		this.POTV1 = (TextView)findViewById(R.id.POTV1);
+		this.POTV1.setTextSize(26);
+		
+		this.POTV2 = (TextView)findViewById(R.id.POTV2);
+		this.POTV2.setTextSize(26);
 	}
 }
