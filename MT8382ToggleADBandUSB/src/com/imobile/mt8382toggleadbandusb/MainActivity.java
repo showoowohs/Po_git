60792d0a585a61a527d50c893d0a400416e91314
Po_Hsun <showoowohs@google.com>
2015-09-16 13:43:36 -0600

    添加 JNI 機制

--- MT8382ToggleADBandUSB/src/com/imobile/mt8382toggleadbandusb/MainActivity.java
+++ MT8382ToggleADBandUSB/src/com/imobile/mt8382toggleadbandusb/MainActivity.java
@@ -2,18 +2,24 @@ package com.imobile.mt8382toggleadbandusb;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private TextView POTV1, POTV2;
 
+	private final String TAG = "Po_dbg";
+
+	static {
+		System.loadLibrary("imobileJNI");
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
 		PO_findID();
+		ReadUSBStatus();
 	}
 	
 	private void PO_findID(){
@@ -22,5 +28,18 @@ public class MainActivity extends Activity {
 		
 		this.POTV2 = (TextView)findViewById(R.id.POTV2);
 		this.POTV2.setTextSize(26);
+	}
+	
+	private String ReadUSBStatus(){
+		String status ="";
+		try {
+			status = imobileJNI.ReadProc("/proc/USBStatus");
+		} catch (Exception e) {
+			// TODO: handle exception
+			return "Read USBStatus error!";
+		}
+		Log.d(TAG, "status="+ status.toString());
+		
+		return status;
 	}
 }
