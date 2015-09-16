0740a3eea297b54ab2afe016b1a28360a37a552d
Po_Hsun <showoowohs@google.com>
2015-09-16 15:48:27 -0600

    layout 雛型大致上完成了

--- MT8382ToggleADBandUSB/src/com/imobile/mt8382toggleadbandusb/MainActivity.java
+++ MT8382ToggleADBandUSB/src/com/imobile/mt8382toggleadbandusb/MainActivity.java
@@ -1,6 +1,8 @@
 package com.imobile.mt8382toggleadbandusb;
 
+import android.R.color;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -13,33 +15,67 @@ public class MainActivity extends Activity {
 	static {
 		System.loadLibrary("imobileJNI");
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		PO_findID();
-		ReadUSBStatus();
+		init();
 	}
-	
-	private void PO_findID(){
-		this.POTV1 = (TextView)findViewById(R.id.POTV1);
-		this.POTV1.setTextSize(26);
+
+	private void PO_findID() {
+		this.POTV1 = (TextView) findViewById(R.id.POTV1);
+		this.POTV2 = (TextView) findViewById(R.id.POTV2);
 		
-		this.POTV2 = (TextView)findViewById(R.id.POTV2);
-		this.POTV2.setTextSize(26);
 	}
-	
+
+	/**
+	 * can read USB status(OTG/USB), through cat /proc/USBStatus
+	 * @return USB current status 
+	 */
 	private String ReadUSBStatus(){
 		String status ="";
+		String ReturnUI ="";
 		try {
 			status = imobileJNI.ReadProc("/proc/USBStatus");
+			Log.d(TAG, "status="+ status.toString());
+			
+			if(status.equals("USB") || status.equals("OTG")){
+				ReturnUI = status + " Model is currently";
+			}else{
+				ReturnUI = "Read USBStatus error!";
+				return ReturnUI;
+			}
 		} catch (Exception e) {
 			// TODO: handle exception
-			return "Read USBStatus error!";
+			ReturnUI = "Read USBStatus error!";
+			return ReturnUI;
 		}
-		Log.d(TAG, "status="+ status.toString());
 		
-		return status;
+		return ReturnUI;
+	}
+	
+	private void init(){
+		// setting text size
+		this.POTV1.setTextSize(36);
+		this.POTV1.setTextColor(Color.BLACK);
+		this.POTV2.setTextSize(30);
+		this.POTV2.setTextColor(Color.BLACK);
+		
+		
+		// show usb mode(USB/OTG)
+		String SetUIText = ReadUSBStatus();
+		this.POTV1.setText(SetUIText);
+		this.POTV2.setText("OTG mode");
+		
+		// check status
+//		Log.d(TAG, "status = "+SetUIText.substring(0, 3));
+//		if(SetUIText.substring(0, 3).equals("USB")){
+//			this.POTV1.setText("USB");
+//		}else if(SetUIText.substring(0, 3).equals("OTG")){
+//			
+//		}
+		
 	}
 }
